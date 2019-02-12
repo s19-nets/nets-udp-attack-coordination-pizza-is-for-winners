34ce73ce08e738540c375326501fb2aa4527573e
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2019-02-12 21:15:22 +0000

    Added tests for packet manager module.

--- packet_manager.py
+++ packet_manager.py
@@ -25,12 +25,53 @@ class Packet_Manager:
     def get_available_messages(self):
         """Gets available messages for either server or client."""
         # Messages available for the server
-        server_msg = ["0, pong", "1, test"]
+        server_msg = ["0, pong", "1, let's coordinate"]
         # Messages available for the client
-        client_msg = ["0, ping", "1, test"]
+        client_msg = ["0, ping", "1, let's coordinate an attack"]
         if self.agent == "server":
             return server_msg
         return client_msg
+
+    def send_requested_message(self, message_num):
+        """Sends the message requested."""
+        if message_num > len(self.get_available_messages()) or message_num < 0:
+            print("Requested message #{} is out of range".format(message_num))
+            return False
+        return self.get_available_messages()[message_num]
+
+    def send_next_message(self):
+        """Will send next expected message."""
+        try:
+            msg = self.get_available_messages()[self.sent_msg]
+            self.sent_msg = self.sent_msg + 1
+            return msg
+        except IndexError:
+            print("No more messages available")
+            return False
+
+    def set_send_msg(self, message_num):
+        """Sets the send message counter to message_num."""
+        if message_num > len(self.get_available_messages()) or message_num < 0:
+            print("Requested message #{} is out of range".format(message_num))
+            return False
+        self.sent_msg = message_num
+        return True
+
+    def get_current_sent_message(self):
+        """Returns current sent message in the packet manager."""
+        try:
+            return self.get_available_messages()[self.sent_msg]
+        except IndexError:
+            print("{} has sent all messages".format(self.agent))
+            return False
+
+    def get_current_received_message(self):
+        """Returns current received message in the packet manager."""
+        print(self.received_msg)
+        if self.received_msg <= 0:
+            print("{} has not received any messages".format(self.agent))
+            return False
+        return self.get_available_messages()[self.received_msg - 1]
 
     def validate_received_message(self, message):
         """Validates that the received message is valid."""
@@ -44,31 +85,6 @@ class Packet_Manager:
         self.received_msg = self.received_msg + 1  # Update current message
         return True
 
-    def send_requested_message(self, message_num):
-        """Sends the message requested."""
-        if message_num > len(self.get_available_messages()):
-            print("Requested message #{} is out of range".format(message_num))
-            raise ValueError
-        return self.get_available_messages()[message_num]
-
-    def send_next_message(self):
-        """Will send next expected message."""
-        try:
-            msg = self.get_available_messages()[self.sent_msg]
-            self.sent_msg = self.sent_msg + 1
-            return msg
-        except ValueError:
-            print("No more messages available")
-            raise ValueError
-
-    def set_send_msg(self, message_num):
-        """Sets the send message counter to message_num."""
-        if message_num > len(self.get_available_messages()):
-            print("Requested message #{} is out of range".format(message_num))
-            raise ValueError
-        self.sent_msg = message_num
-        return True
-
     @staticmethod
     def _prepare_msg(message):
         try:
@@ -76,25 +92,9 @@ class Packet_Manager:
             # Returns tuple with message# and message text
             return (message[0], message[1])
         # Catch exception, and raise it again
-        except ValueError:
+        except IndexError:
             print("Unknown message")
             raise ValueError
-
-    def get_current_received_message(self):
-        """Returns current received message in the packet manager."""
-        print(self.received_msg)
-        if self.received_msg <= 0:
-            print("{} has not received any messages".format(self.agent))
-            return False
-        return self.get_available_messages()[self.received_msg - 1]
-
-    def get_current_sent_message(self):
-        """Returns current sent message in the packet manager."""
-        try:
-            return self.get_available_messages()[self.sent_msg]
-        except ValueError:
-            print("{} has sent all messages".format(self.agent))
-            return False
 
 
 def _main():
--- test_packet_manager.py
+++ test_packet_manager.py
@@ -18,4 +18,143 @@ def test_init():
 
 def test_get_available_messages():
     """Test get_available_messages."""
-    pass
+    client = pm.Packet_Manager("Client")
+    server = pm.Packet_Manager("Server")
+    client_messages = client.get_available_messages()
+    print(client_messages)
+    assert client_messages is not None
+    server_messages = server.get_available_messages()
+    print(server_messages)
+    assert server_messages is not None
+
+
+def test_send_requested_message():
+    """Test send_requested_message."""
+    client = pm.Packet_Manager("Client")
+    client_msgs_len = len(client.get_available_messages())
+
+    # Test incorrect index for requested message (less than size)
+    msg = client.send_requested_message(-1)  # Can't be negative size
+    assert msg is False
+
+    # Test incorrect index for requested message (more than size)
+    msg = client.send_requested_message(client_msgs_len+1)
+    assert msg is False
+
+    # Test correct index (first message)
+    assert client.send_requested_message(0) is not None
+
+
+def test_send_next_message():
+    """Test send_next_message."""
+    client = pm.Packet_Manager("Client")
+
+    # Test first message
+    msg = client.send_next_message()
+    print(msg)
+    assert extract_msg_index(msg) == 0
+
+    # Test second message
+    msg = client.send_next_message()
+    print(msg)
+    assert extract_msg_index(msg) == 1
+
+    # Test out of bounds index message
+    client = pm.Packet_Manager("Client")  # Create a new client
+    for _x in client.get_available_messages():
+        client.send_next_message()
+
+
+def test_set_send_msg():
+    """Test set_send_msg."""
+    client = pm.Packet_Manager("Client")
+    client_msgs_len = len(client.get_available_messages())
+
+    # Check that current messsage nuber is 0
+    msg = client.get_current_sent_message()
+    assert extract_msg_index(msg) == 0
+
+    # Test incorrect index for send index message (less than size)
+    msg = client.set_send_msg(-1)  # Can't be negative size
+    assert msg is False
+
+    # Test incorrect index for send index message (more than size)
+    msg = client.set_send_msg(client_msgs_len+1)
+    assert msg is False
+
+    # Test correct index for send index message
+    msg = client.set_send_msg(0)
+    assert msg is True
+
+
+def test_current_sent_message():
+    """Test current sent message."""
+    client = pm.Packet_Manager("Client")
+    server = pm.Packet_Manager("Server")
+
+    # Test no sent messages
+    assert client.get_current_sent_message() is not False
+    assert server.get_current_sent_message() is not False
+
+    # Send multiple messages to reach the lenght of available messages
+    client_msgs_len = len(client.get_available_messages())
+    server_msgs_len = len(server.get_available_messages())
+    # exhaust client msgs
+    for _x in range(client_msgs_len):
+        client.send_next_message()
+    # exhaust server msgs
+    for _x in range(server_msgs_len):
+        server.send_next_message()
+
+    # Test that no more msgs are available to be sent
+    assert client.get_current_sent_message() is False
+    assert server.get_current_sent_message() is False
+
+
+def test_current_received_message():
+    """Test current received message."""
+    client = pm.Packet_Manager("Client")
+    server = pm.Packet_Manager("Server")
+
+    # Test no received messages
+    assert client.get_current_received_message() is False
+    assert server.get_current_received_message() is False
+
+    # Send one message (server -> client) and test that it was received
+    s_msg = server.send_next_message()
+    client.validate_received_message(s_msg)
+    msg = client.get_current_received_message()
+    assert msg is not False  # A msg was received
+
+    # Send one more message (server -> client) and test that it was received
+    s_msg = server.send_next_message()
+    client.validate_received_message(s_msg)
+    msg = client.get_current_received_message()
+    assert msg is not False  # A msg was received
+
+
+def test_validate_received_message():
+    """Test validate_received_message."""
+    client = pm.Packet_Manager("Client")
+
+    # Validate unknown message
+    with pytest.raises(ValueError):
+        client.validate_received_message("Unknown Message Format")
+
+    # Validate incorrect index
+    assert client.validate_received_message("-1, Incorrect order") is False
+
+    # Validate correct index
+    assert client.validate_received_message("0, Correct msg order") is True
+
+    # Validate next correct index
+    assert client.validate_received_message("1, Correct msg order 2") is True
+
+    # Validate same message index
+    assert client.validate_received_message("1, Correct msg order 2") is False
+
+
+def extract_msg_index(message):
+    """Extracts the index out of a message."""
+    message = message.split(",")
+    return int(message[0])
